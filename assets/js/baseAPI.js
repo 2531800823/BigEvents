f83ea9edfaf5b9f477d0b1e7f76cee88ac5ebb35
2531800823 <2531800823@qq.com>
2021-08-17 23:56:25 +0800

    完成了index的操作

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,5 +2,16 @@
 
 $.ajaxPrefilter(function (options) {
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
-    
+    //判断有 my 要统一给有权限的添加请求头
+    if (options.url.indexOf('/my/') != -1) {
+    options.headers={ Authorization: localStorage.getItem('token')||''}
+    }
+
+    options.complete = function(res) {
+        if(res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！")
+        {
+            localStorage.removeItem('token');
+            location.href='/login.html'
+        }
+    }
 })
